42c0d268d5a01a15febf85d75ce350efebefae57
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2020-02-01 05:04:14 -0600

    [Add] Nav Mesh and Collisions

--- RataKart 8 Deluxe/Assets/Scripts/Move.cs
+++ RataKart 8 Deluxe/Assets/Scripts/Move.cs
@@ -1,55 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Move : MonoBehaviour
 {
-    //Velocidad de moviemiento de personaje
-    public float speed = 1;
-    //Nivel
-    int level = 0;
-    //Rigidbody
-    Rigidbody rb;
+    public Transform goal;
+    NavMeshAgent agent;
 
-    private void Awake()
+    void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
     }
-    // Use this for initialization
-    void Start ()
-    {
-
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-    {
-        AxisInput();
-    }
-
-    //Teclado
-    //Otra Manera de Hacerlo
-    void AxisInput()
-    {
-        //Forward
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.MovePosition(rb.position + Vector3.forward * speed);
-        }
-        //Back
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.MovePosition(rb.position + Vector3.back * speed);
-        }
-        //Left
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.MoveRotation(rb.rotation * Quaternion.Euler(Vector3.up));
-        }
-        //Right
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.MovePosition(rb.position + Vector3.right * speed);
-        }
+    private void FixedUpdate() {
+        agent.destination = goal.position;
     }
 }
