4360c1ee9b60dcf3c6c5bd50f6fbda20e1b4ccbd
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2020-02-01 07:35:31 -0600

    [Add] Nav Mesh

--- RataKart 8 Deluxe/Assets/Scripts/Move.cs
+++ RataKart 8 Deluxe/Assets/Scripts/Move.cs
@@ -7,13 +7,31 @@ public class Move : MonoBehaviour
 {
     public Transform goal;
     NavMeshAgent agent;
-
+    public float horizontalMove;
+    Rigidbody rb;
+    public float speed;
+    Quaternion rotatePos;
+    Vector3 movePos;
+    public float rotationSpeed;
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
     }
-    private void FixedUpdate() {
-        agent.destination = goal.position;
+    private void FixedUpdate()
+    {
+        if (Input.touchCount > 0){
+            float width = Screen.width;
+            Touch touch = Input.GetTouch(0);
+            horizontalMove = (touch.position.x - width/2)/(width/2);
+            transform.Translate(new Vector3(),Space.Self);
+        }
+        else{
+            horizontalMove = 0;
+            agent.destination = goal.position;
+        }
+        rotatePos = Quaternion.Euler(0.0f, horizontalMove * rotationSpeed, 0.0f);
+        rb.MoveRotation(transform.rotation * rotatePos);
+        
     }
 }
