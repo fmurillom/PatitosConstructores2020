9dd0639b3607fea96cd34fb2f3caa822e97705ad
MariaAvila <mariafer50121@gmai.com>
2020-02-01 16:16:18 -0600

    [add] Volume slider functionalities

--- RataKart 8 Deluxe/Assets/Scripts/OptionsMenu.cs
+++ RataKart 8 Deluxe/Assets/Scripts/OptionsMenu.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioMixer audioMixer;
+   public void SetVolume(float volume){
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+       audioMixer.SetFloat("FXLevel", volume);
+
+   }
+
+   public void SetMusic(float volume){
+
+       audioMixer.SetFloat("MusicLevel", volume);
+       
+   }
 }
