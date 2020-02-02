3b98cd693072b675a356aab306bc94e788d06e73
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2020-02-02 01:18:54 -0600

    [Add] Unity Scene for Cinematics

---
+++ RataKart 8 Deluxe/Assets/Scripts/Timeline.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeline : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void start(){
+        Time.timeScale = 1.0f;
+    }
+    public void pause(){
+        Time.timeScale = 0.0f;
+    }
+}
