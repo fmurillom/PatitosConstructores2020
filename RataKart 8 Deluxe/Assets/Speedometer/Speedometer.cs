bae1064b1397728869277a78605467f3f2a9be8b
MariaAvila <mariafer50121@gmai.com>
2020-02-02 09:50:49 -0600

    [add] Initial Speedometer

--- RataKart 8 Deluxe/Assets/Speedometer/Speedometer.cs
+++ RataKart 8 Deluxe/Assets/Speedometer/Speedometer.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections;
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Kart : MonoBehaviour
-{
+public class Speedometer : MonoBehaviour {
+
     private const float MAX_SPEED_ANGLE = -20;
     private const float ZERO_SPEED_ANGLE = 230;
 
@@ -14,10 +26,6 @@ public class Kart : MonoBehaviour
     private float speedMax;
     private float speed;
 
-    private float mouseLife = 100.0f;
-    private float cartLife = 100.0f;
-    private float wheelLife = 100.0f;
-    public Transform Head;
     private void Awake() {
         needleTranform = transform.Find("needle");
         speedLabelTemplateTransform = transform.Find("speedLabelTemplate");
@@ -29,13 +37,12 @@ public class Kart : MonoBehaviour
         CreateSpeedLabels();
     }
 
-    void Update()
-    {
+    private void Update() {
+
         speed += 30f * Time.deltaTime;
         if (speed > speedMax) speed = speedMax;
 
         needleTranform.eulerAngles = new Vector3(0, 0, GetSpeedRotation());
-
     }
 
     private void CreateSpeedLabels() {
@@ -61,53 +68,5 @@ public class Kart : MonoBehaviour
         float speedNormalized = speed / speedMax;
 
         return ZERO_SPEED_ANGLE - speedNormalized * totalAngleSize;
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.transform.tag == "KEZO")
-        {
-            mouseLife = mouseLife + 5.0f;
-        }
-        else if (other.transform.tag == "Bandita")
-        {
-            wheelLife = wheelLife + 5.0f;
-        }
-        else if (other.transform.tag == "Lata")
-        {
-            cartLife = cartLife + 5.0f;
-        }
-        else
-        {
-            Vector3 hit = other.contacts[0].normal;
-            float angle = Vector3.Angle(hit, Vector3.up);
-            //Down
-            if (!Mathf.Approximately(angle, 0))
-            {
-
-                float random = Mathf.Round(Random.Range(0, 2));
-
-                Debug.Log(mouseLife);
-                Debug.Log(cartLife);
-                Debug.Log(wheelLife);
-
-                if (random == 0)
-                {
-                    mouseLife = mouseLife - 5.0f;
-                }
-                if (random == 1)
-                {
-                    cartLife = cartLife - 5.0f;
-                }
-                if (random == 2)
-                {
-                    wheelLife = wheelLife - 5.0f;
-                }
-
-                speed -= 50f;
-                if (speed < 0) speed = 0;
-
-            }
-        }
     }
 }
